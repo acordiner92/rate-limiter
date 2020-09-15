d86ff6e63de481a63edeba950c7317b404bbca72
Andrew Cordiner <andrewcordiner92@gmail.com>
2020-09-15 18:55:15 +1000

    Setup basic express app with /test endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world');
+import express from 'express';
+
+const app = express();
+const port = 5000;
+
+app.get('/test', (_req, res) => res.send('Hello World!'));
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`),
+);
